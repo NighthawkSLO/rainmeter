9170ccf9d9904f4a92741faaa634a71fc1f2809c
Jon <jon.kuhar99@gmail.com>
2016-05-19 19:19:09 +0200

    whitespace

--- Library/MeterVector.cpp
+++ Library/MeterVector.cpp
@@ -23,54 +23,54 @@ MeterVector::~MeterVector()
 
 void MeterVector::Initialize()
 {
-    Meter::Initialize();
+	Meter::Initialize();
 }
 
 bool MeterVector::Update()
 {
-    if (Meter::Update())
-    {
+	if (Meter::Update())
+	{
 
-        return true;
-    }
-    return false;
+		return true;
+	}
+	return false;
 }
 
 bool MeterVector::Draw(Gfx::Canvas & canvas)
 {
 
-    bool canDraw = Meter::Draw(canvas);
-    if (!canDraw) return false;
-    SolidBrush solid(m_fillColor);
-    canvas.DrawPathVector(m_points, solid, m_outlineColor, m_Solid, m_lineWidth, m_connectedEdges);
+	bool canDraw = Meter::Draw(canvas);
+	if (!canDraw) return false;
+	SolidBrush solid(m_fillColor);
+	canvas.DrawPathVector(m_points, solid, m_outlineColor, m_Solid, m_lineWidth, m_connectedEdges);
 
-    return true;
+	return true;
 }
 
 void MeterVector::ReadOptions(ConfigParser & parser, const WCHAR * section)
 {
-    Meter::ReadOptions(parser, section);
-    std::wstring Shapeidentifier = L"Shape";
-    const std::wstring& curShape = parser.ReadString(section, Shapeidentifier.c_str(), L"");
-    size_t i = 0;
-    while (!curShape.empty())
-    {
-        std::vector<std::wstring> shapeTokens = parser.Tokenize(curShape, L"|");
-        for (const std::wstring shape : shapeTokens)
-        {
-            std::vector<std::wstring> pointTokens = parser.Tokenize(parser.ReadString(section, shape.c_str(), L""), L"|");
-            for (const std::wstring point : pointTokens)
-                LogDebug(point.c_str());
-            LogDebug(shape.c_str());
-        }
-        const std::wstring& curShape = parser.ReadString(section, (Shapeidentifier + std::to_wstring(++i)).c_str(), L"");
-    }
+	Meter::ReadOptions(parser, section);
+	std::wstring Shapeidentifier = L"Shape";
+	const std::wstring& curShape = parser.ReadString(section, Shapeidentifier.c_str(), L"");
+	size_t i = 0;
+	while (!curShape.empty())
+	{
+		std::vector<std::wstring> shapeTokens = parser.Tokenize(curShape, L"|");
+		for (const std::wstring shape : shapeTokens)
+		{
+			std::vector<std::wstring> pointTokens = parser.Tokenize(parser.ReadString(section, shape.c_str(), L""), L"|");
+			for (const std::wstring point : pointTokens)
+				LogDebug(point.c_str());
+			LogDebug(shape.c_str());
+		}
+		const std::wstring& curShape = parser.ReadString(section, (Shapeidentifier + std::to_wstring(++i)).c_str(), L"");
+	}
 }
 
 void MeterVector::BindMeasures(ConfigParser& parser, const WCHAR* section)
 {
-    if (BindPrimaryMeasure(parser, section, true))
-    {
-        BindSecondaryMeasures(parser, section);
-    }
+	if (BindPrimaryMeasure(parser, section, true))
+	{
+		BindSecondaryMeasures(parser, section);
+	}
 }
--- Library/MeterVector.h
+++ Library/MeterVector.h
@@ -12,40 +12,40 @@
 #include <vector>
 #include <ole2.h>  // For Gdiplus.h.
 class MeterVector :
-    public Meter
+	public Meter
 {
 public:
-    MeterVector(Skin* skin, const WCHAR* name);
-    virtual ~MeterVector();
+	MeterVector(Skin* skin, const WCHAR* name);
+	virtual ~MeterVector();
 
-    MeterVector(const MeterVector& other) = delete;
-    MeterVector& operator=(MeterVector other) = delete;
+	MeterVector(const MeterVector& other) = delete;
+	MeterVector& operator=(MeterVector other) = delete;
 
-    virtual UINT GetTypeID() { return TypeID<MeterVector>(); }
+	virtual UINT GetTypeID() { return TypeID<MeterVector>(); }
 
-    virtual void Initialize();
-    virtual bool Update();
-    virtual bool Draw(Gfx::Canvas& canvas);
+	virtual void Initialize();
+	virtual bool Update();
+	virtual bool Draw(Gfx::Canvas& canvas);
 
 protected:
-    virtual void ReadOptions(ConfigParser& parser, const WCHAR* section);
-    virtual void BindMeasures(ConfigParser& parser, const WCHAR* section);
+	virtual void ReadOptions(ConfigParser& parser, const WCHAR* section);
+	virtual void BindMeasures(ConfigParser& parser, const WCHAR* section);
 
-    enum VectorType {
-        Rectangle,
-        Circle,
-        Arc,
-        Curve,
-        Custom
-    };
+	enum VectorType {
+		Rectangle,
+		Circle,
+		Arc,
+		Curve,
+		Custom
+	};
 
 private:
-    std::vector<Gfx::Canvas::VectorPoint> m_points;
-    Gdiplus::Color m_fillColor;
-    Gdiplus::Color m_outlineColor;
-    bool m_Solid;
-    bool m_connectedEdges;
-    float m_lineWidth;
+	std::vector<Gfx::Canvas::VectorPoint> m_points;
+	Gdiplus::Color m_fillColor;
+	Gdiplus::Color m_outlineColor;
+	bool m_Solid;
+	bool m_connectedEdges;
+	float m_lineWidth;
 
 };
 #endif
